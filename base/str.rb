b100cdc99ce3edc4eb275b2f24744ce4f87617b0
hz2004 <hz20040@126.com>
2016-07-14 19:36:16 +0800

    practice each_with_index

--- base/str.rb
+++ base/str.rb
@@ -1,4 +1,11 @@
-
+data = ["1","2","3","4","5"]
+s=""
+data.each {|v| s << v << ' and a ' }
+puts s
+puts "join: " + data.join(' and a ')
+s=""
+data.each_with_index {|x,i| s << x; s << " | " if i < data.length - 1}
+puts "each_index: " + s
 
 hash = { "key1" => "val1", "key2" => "val2" }
 string = ""
